fd3c8ce93058823a65f911532f1575d8e8d2963b
Sherone <keynessherone@gmail.com>
2025-09-14 22:02:32 +0530

    db script added

---
+++ db/populatedb.js
@@ -0,0 +1,28 @@
+const { Client } = require("pg");
+
+const SQL = `CREATE TABLE IF NOT EXISTS usernames(
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+username VARCHAR (255)
+
+);
+
+INSERT INTO usernames (username)
+VALUES 
+('Bryan'),
+('Odin'),
+('Damon');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: "postgresql://postgres:sherone@localhost:5432/top_users",
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
